e29c99ef284b41f034505e4d32b710e3c339245a
nanda ricco <nandaricco2677@gmail.com>
2025-02-20 09:36:50 +0700

    prak2

--- mahasiswa14.java
+++ mahasiswa14.java
@@ -7,6 +7,17 @@ public class mahasiswa14 {
     String kelas;
     double ipk;
     
+    public mahasiswa14 (){
+
+    }
+
+    public mahasiswa14 (String nm, String nim, double ipk, String kls){
+        nama = nm;
+        this.nim = nim;
+        this.ipk = ipk;
+        kelas = kls;
+    }
+    
     void tampilkanInformasi(){
         System.out.println("Nama : " + nama);
         System.out.println("NIM  : " + nim);
